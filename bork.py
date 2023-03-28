c8cfafd055ce4d6cff6be1a41c4efd18bfa14bb0
Hannes van Zyl <hannes.vanzyl@platform.sh>
2023-03-28 14:44:08 +0200

    fix sql

--- bork.py
+++ bork.py
@@ -1,5 +1,6 @@
 import base64
 import os
+import sys
 import datetime
 import random
 import logging
@@ -17,8 +18,9 @@ if __name__ == "__main__":
     req_id = base64.b32encode(os.urandom(16)).lower().strip(b"=").decode("utf-8")[:13]
     secret = random.random()
     logging.debug(f'inserting record {req_id}...')
-    db_success,db_res = db.query(mysql_pool, f"insert into `main`.`records` ('req_id', 'secret', 'created') values ( {req_id}, {secret}, DEFAULT(created) )")
+    db_success,db_res = db.query(mysql_pool, f"insert into `main`.`records` (`req_id`, `secret`) values ( '{req_id}', '{secret}' )")
     if db_success:
         logging.debug(f'{req_id} inserted!')
     else:
         logging.error(f'error writing record {req_id}!')
+    sys.exit(0)
