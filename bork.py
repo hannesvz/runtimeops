d8cbe5bd88516e8516943986c7e25dd3ef960994
Hannes van Zyl <hannes.vanzyl@platform.sh>
2023-11-28 22:46:40 +0200

    also print to test a theory

--- bork.py
+++ bork.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     req_id = base64.b32encode(os.urandom(16)).lower().strip(b"=").decode("utf-8")[:13]
     secret = random.random()
     logging.debug(f'inserting record {req_id}...')
+    print(f'print equivalent: inserting record {req_id}...', flush=True)
     db_success,db_res = db.query(mysql_pool, f"insert into `main`.`records` (`req_id`, `secret`) values ( '{req_id}', '{secret}' )")
     if db_success:
         logging.debug(f'{req_id} inserted!')
