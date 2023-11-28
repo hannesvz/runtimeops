c52eb283ba48183fccaef1f27e593f907160192e
Hannes van Zyl <hannes.vanzyl@platform.sh>
2023-11-28 15:43:25 +0200

    try loguru

--- server.py
+++ server.py
@@ -1,34 +1,33 @@
 import json
 import sys
-import logging
-import requests
+from threading import Thread
 
 import flask
 import flask.json
 import gevent.pywsgi
 import pymysql
+import requests
+from flask import request
+from loguru import logger as logging
+from platformshconfig import Config
 
 import db
 
-from flask import request
-from threading import Thread
-
-from platformshconfig import Config
 config = Config()
 
 app = flask.Flask(__name__)
 
-logging.basicConfig(format='%(asctime)s {%(pathname)s:%(lineno)d} %(levelname)s %(message)s', level=config.variable('LOG_LEVEL', 'DEBUG'))
-
 mysql_pool = db.get_mysql_pool()
 
 
-@app.route('/')
+@app.route("/")
 def root():
-    return 'now this is podracing!'
+    logging("hit received on /")
+    return "now this is podracing!"
 
 
 if __name__ == "__main__":
+    logging.info("starting server")
     db.init_db(mysql_pool)
-    http_server = gevent.pywsgi.WSGIServer(('127.0.0.1', int(config.port)), app)
+    http_server = gevent.pywsgi.WSGIServer(("127.0.0.1", int(config.port)), app)
     http_server.serve_forever()
